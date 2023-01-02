6881de92c93e1636e263d7115db669e0ee8d01ae
Michele Richmond <mmrichmond6@gmail.com>
2023-01-02 10:05:45 -0600

    Design Form Complete

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -24,23 +24,7 @@ namespace ExpenseTracker.Controllers
               return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
 
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // GET: Category/AddOrEdit
         public IActionResult AddOrEdit(int id=0)
@@ -71,23 +55,7 @@ namespace ExpenseTracker.Controllers
         }
 
  
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
 
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -108,9 +76,5 @@ namespace ExpenseTracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return _context.Categories.Any(e => e.CategoryId == id);
-        }
     }
 }
