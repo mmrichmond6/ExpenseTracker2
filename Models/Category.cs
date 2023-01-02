c4621293c2fb65d39507c00edb21c059106fa27d
Michele Richmond <mmrichmond6@gmail.com>
2023-01-02 13:58:10 -0600

    Validation added

--- Models/Category.cs
+++ Models/Category.cs
@@ -9,6 +9,7 @@ namespace ExpenseTracker.Models
         public int CategoryId { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
 
         [Column(TypeName = "nvarchar(5)")]
--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -10,16 +10,24 @@ namespace ExpenseTracker.Models
         public int TransactionId { get; set; }
 
         //CategoryId-Foreign Key
+        [Range(1,int.MaxValue,ErrorMessage ="Please select a category")]
         public int CategoryId { get; set; }
 
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than zero.")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon { get { return Category == null ? "" : Category.Icon + " " + Category.Title; } }
+
+        [NotMapped]
+        public string? FormattedAmount { get { return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0"); } }
 
     }
 }
